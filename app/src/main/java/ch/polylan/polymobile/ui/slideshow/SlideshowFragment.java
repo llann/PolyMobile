dab5bf71c7f9521df399df8541b5ef5be5edc97d
Yann Beaud <yann.beaud@polylan.ch>
2025-05-31 03:36:43 +0200

    Fix

--- app/src/main/java/ch/polylan/polymobile/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/ch/polylan/polymobile/ui/slideshow/SlideshowFragment.java
@@ -4,28 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import ch.polylan.polymobile.databinding.FragmentSlideshowBinding;
 
 public class SlideshowFragment extends Fragment {
 
     private FragmentSlideshowBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        SlideshowViewModel slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Access WebView and ProgressBar
+        binding.webview.getSettings().setJavaScriptEnabled(true);
+        binding.webview.loadUrl("[invalid url, do not cite]");
+        binding.webview.setWebViewClient(new android.webkit.WebViewClient() {
+            @Override
+            public void onPageFinished(android.webkit.WebView view, String url) {
+                super.onPageFinished(view, url);
+                binding.progressBar.setVisibility(View.GONE);
+            }
+        });
+
         return root;
     }
 
