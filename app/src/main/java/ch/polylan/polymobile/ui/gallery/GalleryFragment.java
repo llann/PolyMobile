17cceed57cc2cb421f7e53fba5fa22b9594230d5
Yann Beaud <yann.beaud@polylan.ch>
2025-05-31 12:34:41 +0200

    Fix

--- app/src/main/java/ch/polylan/polymobile/ui/gallery/GalleryFragment.java
+++ app/src/main/java/ch/polylan/polymobile/ui/gallery/GalleryFragment.java
@@ -12,7 +12,7 @@ import ch.polylan.polymobile.databinding.FragmentGalleryBinding;
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
-    private static final String BASE_URL = "[invalid url, do not cite]";
+    private static final String BASE_URL = "https://polylan.ch";
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
