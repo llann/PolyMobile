b648adbcb3c276858c2c77fbd85864713c4692f2
Yann Beaud <yann.beaud@polylan.ch>
2025-05-31 03:20:25 +0200

    Fix

--- app/src/main/java/ch/polylan/polymobile/MenusFragment.java
+++ app/src/main/java/ch/polylan/polymobile/MenusFragment.java
@@ -1,25 +1,38 @@
-private FragmentHomeBinding binding;
+package ch.polylan.polymobile;
 
-public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    binding = FragmentHomeBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import ch.polylan.polymobile.databinding.FragmentHomeBinding;
 
-    // Access WebView and ProgressBar
-    binding.webview.getSettings().setJavaScriptEnabled(true);
-    binding.webview.loadUrl("[invalid url, do not cite]");
-    binding.webview.setWebViewClient(new android.webkit.WebViewClient() {
-        @Override
-        public void onPageFinished(android.webkit.WebView view, String url) {
-            super.onPageFinished(view, url);
-            binding.progressBar.setVisibility(View.GONE);
-        }
-    });
+public class MenusFragment extends Fragment {
 
-    return root;
-}
+    private FragmentHomeBinding binding;
 
-@Override
-public void onDestroyView() {
-    super.onDestroyView();
-    binding = null;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        // Access WebView and ProgressBar
+        binding.webview.getSettings().setJavaScriptEnabled(true);
+        binding.webview.loadUrl("[invalid url, do not cite]");
+        binding.webview.setWebViewClient(new android.webkit.WebViewClient() {
+            @Override
+            public void onPageFinished(android.webkit.WebView view, String url) {
+                super.onPageFinished(view, url);
+                binding.progressBar.setVisibility(View.GONE);
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
