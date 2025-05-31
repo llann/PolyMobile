893035f0aa250b9c9a5f4f4fdddeae8869bbed39
Yann Beaud <yann.beaud@polylan.ch>
2025-05-31 02:58:19 +0200

    Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,21 @@
 plugins {
     id("com.android.application")
 }
+
 android {
     namespace = "ch.polylan.polymobile"
     compileSdk = 34
+
     defaultConfig {
         applicationId = "ch.polylan.polymobile"
         minSdk = 34
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -23,11 +27,17 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 }
+
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.webkit:webkit:1.12.0")
+    implementation("androidx.webkit:webkit:1.12.1")
+    implementation("androidx.navigation:navigation-fragment:2.8.3")
+    implementation("androidx.navigation:navigation-ui:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-common:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-runtime:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.8.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
