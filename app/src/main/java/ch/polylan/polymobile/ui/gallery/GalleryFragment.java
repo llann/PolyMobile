c38dcccabebb1e807c80545367521b79269dffe6
Yann Beaud <yann.beaud@polylan.ch>
2025-05-31 03:29:40 +0200

    Fix

--- app/src/main/java/ch/polylan/polymobile/ui/gallery/GalleryFragment.java
+++ app/src/main/java/ch/polylan/polymobile/ui/gallery/GalleryFragment.java
@@ -4,28 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import ch.polylan.polymobile.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Access WebView and ProgressBar
+        binding.webview.getSettings().setJavaScriptEnabled(true);
+        binding.webview.loadUrl("[invalid url, do not cite]");
+        binding.webview.setWebViewClient(new android.webkit.WebViewClient() {
+            @Override
+            public void onPageFinished(android.webkit.WebView view, String url) {
+                super.onPageFinished(view, url);
+                binding.progressBar.setVisibility(View.GONE);
+            }
+        });
+
         return root;
     }
 
